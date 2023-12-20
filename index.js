21ae2d8243360df7278dabff87a3732d9f5156f0
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-12-20 21:23:22 +0000

    background fade and scoreboard lit up

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Variables
+const gameContainer = document.querySelector(".game");
 const container = document.querySelector(".container");
 const startButton = document.querySelector(".start-button");
 const cursor = document.querySelector(".cursor");
-const buttonContainer = document.querySelector('.button-container');
-const scoreBox = document.querySelector('.score-box');
-const timerDisplay = document.getElementById('timer');
-const scoreCard = document.querySelector('.scoreboard-score')
+const buttonContainer = document.querySelector(".button-container");
+const scoreBoard = document.querySelector(".scoreboard");
+const timerDisplay = document.getElementById("timer");
+const scoreCard = document.querySelector(".scoreboard-score");
+
 // Function to handle space bar press on Intro screen
 function handleKeyPress(event) {
   if (event.code === "Space") {
@@ -45,15 +47,15 @@ window.addEventListener("mousemove", (e) => {
 });
 
 // Sounds
-const baubleSound       = new Audio('./assets/woosh-bauble-throw (1).mp3')
-const baubleSound2      = new Audio('assets/woosh-bauble-throw-2.mp3')
-const gameStartSound    = new Audio('./assets/gamestart.mp3')
-const altBurglarSound    = new Audio('./assets/keith_lemon_potato.mp3');
-const ouchSound1        = new Audio('./assets/ouch1.mp3')
-const ouchSound2        = new Audio('./assets/ouch2.mp3')
-const missedSound       = new Audio('./assets/missed1.mp3')
-const missedSound2      = new Audio('./assets/missed2.mp3')
-let randomSound
+const baubleSound = new Audio("./assets/woosh-bauble-throw (1).mp3");
+const baubleSound2 = new Audio("assets/woosh-bauble-throw-2.mp3");
+const gameStartSound = new Audio("./assets/gamestart.mp3");
+const altBurglarSound = new Audio("./assets/keith_lemon_potato.mp3");
+const ouchSound1 = new Audio("./assets/ouch1.mp3");
+const ouchSound2 = new Audio("./assets/ouch2.mp3");
+const missedSound = new Audio("./assets/missed1.mp3");
+const missedSound2 = new Audio("./assets/missed2.mp3");
+let randomSound;
 
 // Burglar
 const burglar = document.createElement("img");
@@ -63,7 +65,7 @@ burglar.setAttribute("src", "./assets/burglar.png");
 // Bauble
 let baubleCounter = 0;
 const bauble = document.querySelector(".bauble");
-bauble.style.display = 'none'
+bauble.style.display = "none";
 
 // Positons and measurements
 const containerHeight = container.offsetHeight;
@@ -72,41 +74,40 @@ const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
 
 // Start Game
-function startGame(){
+function startGame() {
   gameStarted = true;
   container.appendChild(burglar);
-  burglar.style.animationName = 'bounceIn';
+  burglar.style.animationName = "bounceIn";
   clearInterval(intervalId);
   setRandomPosition();
-  startButton.innerText = 'Reset Game';
-  burglar.style.display = 'block';
-  gameStartSound.play()
+  startButton.innerText = "Reset Game";
+  burglar.style.display = "block";
+  gameStartSound.play();
   timerInterval = setInterval(updateTimer, 1000);
-
 }
 
 // move the bauble cannon with the mouse
-const present = document.querySelector('.present'); 
-window.addEventListener('mousemove', (event) => {
-  const presentRect = present.getBoundingClientRect();
-  const presentCenterX = presentRect.left + presentRect.width / 2;
-  const distanceFromCenter = event.clientX - presentCenterX;
-  const rotationAngle = distanceFromCenter / 10; 
-  present.style.transform = `rotate(${rotationAngle}deg)`;
+const cannon = document.querySelector(".cannon");
+window.addEventListener("mousemove", (event) => {
+  const cannonRect = cannon.getBoundingClientRect();
+  const cannonCenterX = cannonRect.left + cannonRect.width / 2;
+  const distanceFromCenter = event.clientX - cannonCenterX;
+  const rotationAngle = distanceFromCenter / 10;
+  cannon.style.transform = `rotate(${rotationAngle}deg)`;
 });
 
 // Reset the game
 function resetGame() {
-  clearInterval(timerInterval)
+  clearInterval(timerInterval);
   gameStarted = false;
   timer = 30;
   score = 0;
-  startButton.innerText = 'Start Game';
+  startButton.innerText = "Start Game";
   scoreCard.innerText = `${score}`;
   container.removeChild(burglar);
   timerDisplay.innerText = 30;
   burglarSpeed = 1500;
-  bauble.style.display = 'none';
+  bauble.style.display = "none";
   bauble.style.bottom = 0;
 }
 
@@ -117,7 +118,8 @@ function setBurglarInterval() {
 }
 
 function setBurglarAppearance() {
-  if (Math.random() < 0.2) { // 
+  if (Math.random() < 0.2) {
+    //
     burglar.setAttribute("src", "./assets/burglar-paddy.png");
     burglar.dataset.isAlternative = "true";
   } else {
@@ -129,7 +131,7 @@ function setBurglarAppearance() {
 // Move the burglar to random positions on the screen
 function moveBurglar() {
   setBurglarAppearance();
-  const containerRect = container.getBoundingClientRect()
+  const containerRect = container.getBoundingClientRect();
   const randTop = Math.random() * (containerRect.height - 100);
   const randLeft = Math.random() * (containerRect.width - 100);
 
@@ -149,8 +151,11 @@ function updateTimer() {
   timerDisplay.innerText = timer;
 
   if (timer === 0) {
-    clearInterval(timerInterval)
-    alert(`Time's up! Your final score is ${score}`)
+    clearInterval(timerInterval);
+    const overlay = document.querySelector(".overlay");
+    overlay.style.background = "rgba(0, 0, 0, 0.8)";
+    scoreBoard.style.transform = "translate(-90%, 28%)";
+    scoreBoard.style.transition = "all 2s ease-in-out";
     resetGame();
   }
 }
@@ -165,11 +170,11 @@ startButton.addEventListener("click", () => {
 });
 
 // Clicking the burglar scores a point
-burglar.addEventListener('click', (event) => {
+burglar.addEventListener("click", (event) => {
   if (event.target === burglar) {
     score++;
     scoreCard.innerText = `${score}`;
-    burglarSpeed -= 60
+    burglarSpeed -= 60;
     if (burglar.dataset.isAlternative === "true") {
       altBurglarSound.play();
     } else {
@@ -177,42 +182,44 @@ burglar.addEventListener('click', (event) => {
       hitSound.play();
     }
     moveBurglar();
-    setBurglarInterval(); 
+    setBurglarInterval();
   }
 });
 
-// reset bauble 
+// reset bauble
 function resetBaublePosition() {
-  bauble.style.display = 'block';
-  bauble.style.bottom = '0';
-  bauble.style.left = '50%';
-  bauble.style.transform = 'translateX(-50%)';
+  bauble.style.display = "block";
+  bauble.style.bottom = "0";
+  bauble.style.left = "50%";
+  bauble.style.transform = "translateX(-50%)";
 }
 
 // Moving the bauble on click
 function mouseClicked(event) {
   if (gameStarted) {
     resetBaublePosition();
-    bauble.style.display = 'block'
-    const xposition = (event.clientX - bauble.offsetLeft - bauble.offsetWidth / 2);
-    const yposition = (event.clientY - bauble.offsetTop - bauble.offsetHeight / 2);
+    bauble.style.display = "block";
+    const xposition =
+      event.clientX - bauble.offsetLeft - bauble.offsetWidth / 2;
+    const yposition =
+      event.clientY - bauble.offsetTop - bauble.offsetHeight / 2;
     randomSound = Math.random() < 0.5 ? baubleSound : baubleSound2;
     randomSound.play();
-    if (bauble.style.bottom === '') bauble.style.bottom = '0';
-    
+    if (bauble.style.bottom === "") bauble.style.bottom = "0";
+
     // Add CSS transition for smooth movement
-    bauble.style.transition = 'transform 0.4s ease-out'; 
+    bauble.style.transition = "transform 0.4s ease-out";
     bauble.style.transform = `translate(${xposition}px, ${yposition}px)`;
   }
 }
 
 // hide bauble at the end of its throw
-bauble.addEventListener('transitionend', () => {
-  bauble.style.display = 'none'; 
+bauble.addEventListener("transitionend", () => {
+  bauble.style.display = "none";
 });
 
-window.addEventListener('mousemove', () => {
-    if (gameStarted && container.contains(burglar)) {
-    container.addEventListener('click', mouseClicked);
+window.addEventListener("mousemove", () => {
+  if (gameStarted && container.contains(burglar)) {
+    container.addEventListener("click", mouseClicked);
   }
-})
+});
